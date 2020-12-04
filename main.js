c8dc49510e977e556b1eccaf85d6207ce306020f
Tracy Falba <tfalba@mac.com>
2020-12-04 08:56:59 -0500

    made formatting changes, added header and colors, increased font size so only goes to 44 students for now

--- main.js
+++ main.js
@@ -6,8 +6,10 @@ const numberStudents = document.querySelector('#total-students')
 const round = document.querySelector('#round')
 const form = document.querySelector('#form')
 const display = document.querySelector('#display')
-const sideBar = document.querySelector('#side-bar')
+const sideBar = document.querySelector('.side-bar')
 const dropStudents = document.querySelector('#drop-students')
+const boxA = document.querySelector('#boxa')
+const boxB = document.querySelector('#boxb')
 
 form.addEventListener('submit', function (event) {
   event.preventDefault()
@@ -80,27 +82,16 @@ form.addEventListener('submit', function (event) {
     studentArray1.push(50)
   }
   studentArrays[0] = studentArray1
-  // debugger
-
-  // set totalStudents = finalStudents.length
-  //change out references to studentArray1 to finalStudents
 
   const shifter = totalStudents / 2
   for (let i = 1; i < studentArrays.length; i++) {
-    // studentArrays[i].push(studentArrays[i - 1][studentArrays[i - 1].length - 1])
     studentArrays[i].push(studentArrays[0][0])
     for (let j = 1; j < (totalStudents); j++) {
       if (j < shifter) {
         studentArrays[i].push(studentArrays[i - 1][j + shifter])
-      }
-      else {
+      } else {
         studentArrays[i].push(studentArrays[i - 1][j - shifter + 1])
       }
-
-      // console.log(studentArrays)
-      // console.log(studentArrays[i])
-      // console.log(studentArrays[i][j])
-      // console.log(i)
     }
   }
 
@@ -109,36 +100,50 @@ form.addEventListener('submit', function (event) {
     boxes[i].classList.remove('hideme')
   }
 
-  for (let i = shifter; i < 50 - shifter; i++) {
+  // for (let i = shifter; i < 50 - shifter; i++) {
+
+  for (let i = shifter; i < 44 - shifter; i++) {
     boxes[i].classList.add('hideme')
   }
 
-  for (let i = 50 - shifter; i < 50; i++) {
-    boxes[i].innerHTML = studentArrays[round.value - 1][i - (50 - totalStudents)]
+  // for (let i = 50 - shifter; i < 50; i++) {
+  for (let i = 44 - shifter; i < 44; i++) {
+  //  boxes[i].innerHTML = studentArrays[round.value - 1][i - (50 - totalStudents)]
+    boxes[i].innerHTML = studentArrays[round.value - 1][i - (44 - totalStudents)]
     boxes[i].classList.remove('hideme')
   }
 
-  // change above to go to shifter -- from shifter to 25 set to hideme
-  // then hide from 26 for (50-shifter/2)
-  // close to above but see post it note.
-  // need to have two nested loops each counting by 2.
-
-  // for (let i = shifter; i < 50 - shifter - 1; i++) {
-  //   boxes[i].classList.add('hideme')
-  // }
-
-  if (round.value === '1' | round.value === '6' | round.value === '11' | round.value === '16') {
-    display.classList.add('round1')
+  if (round.value === '1' | round.value === '6' | round.value === '11' | round.value === '16' | round.value === '21' | round.value === '26') {
+    sideBar.classList.add('round1')
+    boxA.classList.add('round1')
+    boxB.classList.add('round1')
   }
-  if (round.value === '2' | round.value === '7' | round.value === '12' | round.value === '17') {
-    display.classList.add('round2')
+  if (round.value === '2' | round.value === '7' | round.value === '12' | round.value === '17' | round.value === '22' | round.value === '27') {
+    sideBar.classList.add('round2')
+    boxA.classList.add('round2')
+    boxB.classList.add('round2')
   }
+
+  if (round.value === '3' | round.value === '8' | round.value === '13' | round.value === '18' | round.value === '23' | round.value === '28') {
+    sideBar.classList.add('round3')
+    boxA.classList.add('round3')
+    boxB.classList.add('round3')
+  }
+  if (round.value === '4' | round.value === '9' | round.value === '14' | round.value === '19' | round.value === '24') {
+    sideBar.classList.add('round4')
+    boxA.classList.add('round4')
+    boxB.classList.add('round4')
+  }
+  if (round.value === '5' | round.value === '10' | round.value === '15' | round.value === '20' | round.value === '25') {
+    sideBar.classList.add('round5')
+    boxA.classList.add('round5')
+    boxB.classList.add('round5')
+  }
+
+
   sideBar.innerHTML = `ROUND ${round.value}`
   console.log(studentArrays[3])
 })
-
-// //   form.classList.add('hideme')  -- use this as a way to shift content of the page.
-// //    could add a varname.classList.remove('hideme') to whatever was in background
 
 const box1 = document.querySelector('#box1')
 const box2 = document.querySelector('#box2')
@@ -184,14 +189,20 @@ const box41 = document.querySelector('#box41')
 const box42 = document.querySelector('#box42')
 const box43 = document.querySelector('#box43')
 const box44 = document.querySelector('#box44')
-const box45 = document.querySelector('#box45')
-const box46 = document.querySelector('#box46')
-const box47 = document.querySelector('#box47')
-const box48 = document.querySelector('#box48')
-const box49 = document.querySelector('#box49')
-const box50 = document.querySelector('#box50')
-const boxes = [box1, box2, box3, box4, box5, 
-  box6, box7, box8, box9, box10,
-  box11, box12, box13, box14, box15, box16, box17, box18, box19, box20, box21, box22, box23, box24,
-  box25, box26, box27, box28, box29, box30, box31, box32, box33, box34, box35, box36, box37, box38,
-  box39, box40, box41, box42, box43, box44, box45, box46, box47, box48, box49, box50]
+// const box45 = document.querySelector('#box45')
+// const box46 = document.querySelector('#box46')
+// const box47 = document.querySelector('#box47')
+// const box48 = document.querySelector('#box48')
+// const box49 = document.querySelector('#box49')
+// const box50 = document.querySelector('#box50')
+// const boxes = [box1, box2, box3, box4, box5, 
+//   box6, box7, box8, box9, box10,
+//   box11, box12, box13, box14, box15, box16, box17, box18, box19, box20, box21, box22, box23, box24,
+//   box25, box26, box27, box28, box29, box30, box31, box32, box33, box34, box35, box36, box37, box38,
+//   box39, box40, box41, box42, box43, box44, box45, box46, box47, box48, box49, box50]
+
+  const boxes = [box1, box2, box3, box4, box5, 
+    box6, box7, box8, box9, box10,
+    box11, box12, box13, box14, box15, box16, box17, box18, box19, box20, box21, box22, box23, box24,
+    box25, box26, box27, box28, box29, box30, box31, box32, box33, box34, box35, box36, box37, box38,
+    box39, box40, box41, box42, box43, box44]
